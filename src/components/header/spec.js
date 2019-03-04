e098bf3a6f4c274d59b952639e00aaba2c1bde32
vijar50 <40033437+vijar50@users.noreply.github.com>
2019-03-04 15:26:38 +0000

    Header component and Tests

---
+++ src/components/header/spec.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Header from "./index";
+import { findByTestAttr } from "../../../Utils";
+
+//Do a shallow render of the Header component, parsing props
+//return component
+const setUp = (props = {}) => {
+  const component = shallow(<Header {...props} />);
+  return component;
+};
+
+//Header Component Test
+describe("Header Component", () => {
+  let component;
+
+  //Before each test is run
+  beforeEach(() => {
+    component = setUp();
+  });
+
+  //Test Step
+  it("Should render without errors", () => {
+    //This console log will output HTML of Header component
+    // console.log(component.debug());
+    //Find .headerComponent class, store it in wrapper
+    const wrapper = findByTestAttr(component, "headerComponent");
+    //3. Check that wrapper exists on the page
+    expect(wrapper.length).toBe(1);
+  });
+
+  it("Should render a logo", () => {
+    const logo = findByTestAttr(component, "logoIMG");
+    expect(logo.length).toBe(1);
+  });
+});
