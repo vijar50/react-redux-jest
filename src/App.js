926aa78c691a719a4618dc71ed11c0a61df7eb20
vijar50 <40033437+vijar50@users.noreply.github.com>
2019-03-05 11:31:54 +0000

    Headline component with tests

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from "./components/header";
+import Headline from "./components/headline";
 import "./app.scss";
 
 class App extends Component {
@@ -7,6 +8,9 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
+        <section className="main">
+          <Headline header="Posts" desc="Click the button to render posts" />
+        </section>
       </div>
     );
   }
